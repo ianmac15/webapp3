2d9a1d1cb5479b314e9f84e1e9d87cdf280ef1d2
ianmac15 <3thita@gmail.com>
2022-02-03 18:20:03 +0200

    7

--- car-app/src/App.js
+++ car-app/src/App.js
@@ -1,13 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import UserComponent from './components/UserComponent';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <UserComponent/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cars: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('api/car');
+    const body = await response.json();
+    this.setState({ cars: body });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Cars List</h1>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <td> Car Id</td>
+              <td> Car Brand</td>
+              <td> Car Model</td>
+              <td> Car Colour</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.cars.map(
+                car =>
+                  <tr key={car.id}>
+                    <td> {car.id}</td>
+                    <td> {car.brand}</td>
+                    <td> {car.model}</td>
+                    <td> {car.colour}</td>
+                  </tr>
+              )
+            }
+          </tbody>
+
+        </table>
+      </div>
+    )
+  }
 }
-
 export default App;
